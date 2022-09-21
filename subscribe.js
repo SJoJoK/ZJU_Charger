390e7520380d689b2d150a6d15cc93e1efe1fd94
SJoJoK <450986321@qq.com>
2022-09-21 22:42:42 +0800

    修改多hook支持

--- subscribe.js
+++ subscribe.js
@@ -4,61 +4,55 @@ const moment = require('moment')
 const secret = require('./secret.json')
 const config = require('./config.json')
 const campusLocations = config.campusLocations
+const postWebhookInstances = {}
+
 var token = true
-const processRes = (res) => {
-    const prefix = "请热心同学将教职工专属充电区域发送至csjk@zju.edu.cn\n空桩信息(" + moment().utc().add(8, 'h').format("HH:mm") + "):\n"
-    if (res.data.code == 5001) {
-        token = false
-        text = "token已过期，请联系管理员"
-        postWebhookInstance.post('', {
-            "msgtype": "text",
-            "text": {
-                "content": prefix + text
-            },
-            "at": {
-                "atMobiles": [
-                    secret.admin
-                ]
-            },
-        })
-        postWebhookInstance2.post('', {
-            "msgtype": "text",
-            "text": {
-                "content": prefix + text
-            },
-            "at": {
-                "atMobiles": [
-                    secret.admin
-                ]
-            },
-        })
-    }
-    else {
-        text = processList(res.data.data)
-            .sort((a, b) => (b.totalFreeNumber - a.totalFreeNumber))
-            .map((info, index) => (info.areaName + ':' + info.totalFreeNumber + '\n')).toString().replaceAll(',', '')
-        postWebhookInstance.post('', {
-            "msgtype": "text",
-            "text": {
-                "content": prefix + text
-            }
-        })
-        postWebhookInstance2.post('', {
-            "msgtype": "text",
-            "text": {
-                "content": prefix + text
-            }
-        })
+const getResProcess = (campus) => {
+    return (res) => {
+        const prefix = "请热心同学将教职工专属充电区域发送至csjk@zju.edu.cn\n空桩信息(" + moment().utc().add(8, 'h').format("HH:mm") + "):\n"
+        if (res.data.code == 5001) {
+            token = false
+            text = "token已过期，请联系管理员"
+            postWebhookInstances[campus].forEach((instance) => {
+                instance.post('', {
+                    "msgtype": "text",
+                    "text": {
+                        "content": prefix + text
+                    },
+                    "at": {
+                        "atMobiles": [
+                            secret.admin
+                        ]
+                    },
+                })
+            })
+        }
+        else {
+            text = getListProcess(campus)(res.data.data)
+                .sort((a, b) => (b.totalFreeNumber - a.totalFreeNumber))
+                .map((info, index) => (info.areaName + ':' + info.totalFreeNumber + '\n')).toString().replaceAll(',', '')
+            postWebhookInstances[campus].forEach((instance) => {
+                instance.post('', {
+                    "msgtype": "text",
+                    "text": {
+                        "content": prefix + text
+                    }
+                })
+            })
+        }
 
     }
+}
 
 
+const getListProcess = (campus) => {
+    return (list = []) => {
+        const freeAreas = list.filter((info, index) => info.totalFreeNumber > 0 && config.blocks.indexOf(info.areaName) < 0 && info.chargingProjectName == campus)
+            .map((info, index) => ({ areaName: info.areaName, totalFreeNumber: info.totalFreeNumber }))
+        return freeAreas
+    }
 }
-const processList = (list = []) => {
-    const freeAreas = list.filter((info, index) => info.totalFreeNumber > 0 && config.blocks.indexOf(info.areaName) < 0)
-        .map((info, index) => ({ areaName: info.areaName, totalFreeNumber: info.totalFreeNumber }))
-    return freeAreas
-}
+
 const reqChargerInstance = axios.create({
     baseURL: 'https://gateway.hzxwwl.com/api/charging/pile/listCircleChargingArea',
     headers: {
@@ -68,19 +62,21 @@ const reqChargerInstance = axios.create({
         ...campusLocations["浙江大学玉泉校区"],
     },
 })
-const postWebhookInstance = axios.create({
-    baseURL: config.test ? secret.testWebhook : secret.Webhook,
-    headers: {
-        "Content-Type": "application/json ;charset=utf-8"
-    }
-})
 
-const postWebhookInstance2 = axios.create({
-    baseURL: config.test ? secret.testWebhook : secret.Webhook2,
-    headers: {
-        "Content-Type": "application/json ;charset=utf-8"
-    }
-})
+const getPostWebhookInstance = (webhook) => {
+    return axios.create({
+        baseURL: config.test ? secret.testWebhook : secret[webhook],
+        headers: {
+            "Content-Type": "application/json ;charset=utf-8"
+        }
+    })
+}
+
+postWebhookInstances.浙江大学玉泉校区 = []
+postWebhookInstances.浙江大学玉泉校区.push(getPostWebhookInstance("testWebhook"))
+    
+postWebhookInstances.浙江大学紫金港校区 = []
+postWebhookInstances.浙江大学紫金港校区.push(getPostWebhookInstance("testWebhook"))
 
 const reqCampus = (campus) => {
     return reqChargerInstance.get('', {
@@ -90,9 +86,11 @@ const reqCampus = (campus) => {
 
     })
 }
+
 const handler = (campus) => {
-    reqCampus(campus).then(processRes)
+    reqCampus(campus).then(getResProcess(campus))
 }
+
 const getHandler = (campus) => {
     let currentTime = moment().utc();
     let beginningTime = moment().utc().startOf('d').subtract(2, 'h')
