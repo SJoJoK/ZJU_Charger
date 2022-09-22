66861297776ab244da431b0320beea2c43fdf477
SJoJoK <450986321@qq.com>
2022-09-22 21:06:46 +0800

    增加了空桩编号

--- subscribe.js
+++ subscribe.js
@@ -11,44 +11,41 @@ const postWebhookInstances = {}
 const webhookForYQ = ["Webhook", "Webhook2", "YQWebhook"]
 const webhookForZJG = ["Webhook", "Webhook2", "ZJGWebhook"]
 
-var token = ''
+var tmpToken = ''
 var hasToken = true
 
 const getResProcess = (campus) => {
     const prefix = "空桩信息:\n"
     const postfix = "请热心同学将教职工专属充电区域发送至csjk@zju.edu.cn"
-    return (res) => {
+    return async (res) => {
         const resTime = "(查询时间:" + moment(res.headers.date).utc().add(8, 'h').format("HH:mm:ss") + ")\n"
         if (res.data.code == 5001) {
             hasToken = false
             text = "token已过期，请联系管理员"
-            postWebhookInstances[campus].forEach((instance) => {
-                instance.post('', {
-                    "msgtype": "text",
-                    "text": {
-                        "content": prefix + resTime + text + postfix
-                    },
-                    "at": {
-                        "atMobiles": [
-                            secret.admin
-                        ]
-                    },
-                })
-            })
         }
         else {
-            text = getListProcess(campus)(res.data.data)
-                .sort((a, b) => (b.totalFreeNumber - a.totalFreeNumber))
-                .map((info, index) => (info.areaName + ':' + info.totalFreeNumber + '\n')).toString().replaceAll(',', '')
-            postWebhookInstances[campus].forEach((instance) => {
-                instance.post('', {
-                    "msgtype": "text",
-                    "text": {
-                        "content": prefix + resTime + text + postfix
+            const areaInfos = await Promise.all(getListProcess(campus)(res.data.data).sort((a, b) => (b.totalFreeNumber - a.totalFreeNumber)).map(async info =>
+                info.totalFreeNumber >= 4 ? info :
+                    {
+                        ...info,
+                        freePiles: await getReqChargerInstance(info.id).get().then(res => res.data.data.chargingPileList.filter(chargerInfo => chargerInfo.status == 0).map(chargerInfo => chargerInfo.pileNumber))
                     }
-                })
-            })
+            ))
+            oldtext = areaInfos.map((info, index) => (info.areaName + ':' + info.totalFreeNumber + '\n'))
+                .toString()
+                .replaceAll(',', '')
+            text = areaInfos.map(info => info.totalFreeNumber >= 4 ? info.areaName + ': ' + info.totalFreeNumber + '个\n' :
+                info.areaName + ': ' + info.freePiles.map((id,idx)=>idx==0?id:'、'+id)+'号\n')
+                .toString().replaceAll(',', '')
         }
+        postWebhookInstances[campus].forEach((instance) => {
+            instance.post('', {
+                "msgtype": "text",
+                "text": {
+                    "content": prefix + resTime + text + postfix
+                }
+            })
+        })
     }
 }
 
@@ -56,12 +53,12 @@ const getResProcess = (campus) => {
 const getListProcess = (campus) => {
     return (list = []) => {
         const freeAreas = list.filter((info, index) => info.totalFreeNumber > 0 && config.blocks.indexOf(info.areaName) < 0 && info.chargingProjectName == campus)
-            .map((info, index) => ({ areaName: info.areaName, totalFreeNumber: info.totalFreeNumber }))
+            .map((info, index) => ({ areaName: info.areaName, totalFreeNumber: info.totalFreeNumber, id: info.id }))
         return freeAreas
     }
 }
 
-const reqChargerInstance = axios.create({
+const reqCampusInstance = axios.create({
     baseURL: 'https://gateway.hzxwwl.com/api/charging/pile/listCircleChargingArea',
     params: {
         ...campusLocations["浙江大学玉泉校区"],
@@ -76,21 +73,25 @@ const reqTokenInstance = axios.create({
     }
 })
 
-const getPostWebhookInstance = (webhook) => {
-    return axios.create({
-        baseURL: config.test ? secret.testWebhook : secret[webhook],
-        headers: {
-            "Content-Type": "application/json ;charset=utf-8"
-        }
-    })
-}
+const getReqChargerInstance = (areaId, token = tmpToken) => axios.create({
+    baseURL: 'https://gateway.hzxwwl.com/api/charging/pile/listChargingPileDistByArea',
+    headers: {
+        "REQ-NPD-TOKEN": token
+    },
+    params: {
+        chargingAreaId: areaId
+    }
+})
 
-postWebhookInstances.浙江大学玉泉校区 = webhookForYQ.map(webhook => getPostWebhookInstance(webhook))
+const getPostWebhookInstance = (webhook) => axios.create({
+    baseURL: config.test ? secret.testWebhook : secret[webhook],
+    headers: {
+        "Content-Type": "application/json ;charset=utf-8"
+    }
+})
 
-postWebhookInstances.浙江大学紫金港校区 = webhookForZJG.map(webhook => getPostWebhookInstance(webhook))
-
-const reqCampus = (campus, token) => {
-    return reqChargerInstance.get('', {
+const reqCampus = (campus, token = tmpToken) => {
+    return reqCampusInstance.get('', {
         headers: {
             "REQ-NPD-TOKEN": token
         },
@@ -105,8 +106,9 @@ const handler = (campus) => {
     const beginningTime = moment().utc().add(8, 'h').startOf('d').add(6, 'h')
     if (currentTime.isAfter(beginningTime)) {
         reqTokenInstance.get().then(res => {
-            reqCampus(campus,res.data.data.token).then(getResProcess(campus))
-        })  
+            tmpToken = res.data.data.token
+            reqCampus(campus, tmpToken).then(getResProcess(campus))
+        })
     }
 }
 
@@ -116,7 +118,12 @@ const getHandler = (campus) => {
     }
 }
 
-getHandler("浙江大学玉泉校区")()
-// getHandler("浙江大学紫金港校区")()
+
+postWebhookInstances.浙江大学玉泉校区 = webhookForYQ.map(webhook => getPostWebhookInstance(webhook))
+
+postWebhookInstances.浙江大学紫金港校区 = webhookForZJG.map(webhook => getPostWebhookInstance(webhook))
+
+// getHandler("浙江大学玉泉校区")()
+getHandler("浙江大学紫金港校区")()
 // const intervalYQ = setInterval(getHandler("浙江大学玉泉校区"), config.period * 1000)
 // const intervalZJG = setInterval(getHandler("浙江大学紫金港校区"), config.period * 1000)
